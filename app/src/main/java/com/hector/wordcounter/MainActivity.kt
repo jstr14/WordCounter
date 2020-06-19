d6ab2b68fa3d1314b37923310c380f14ed2a4c7b
Hector <hector@helm.cat>
2020-06-19 13:52:56 +0200

    Add Intent to select folder

--- app/src/main/java/com/hector/wordcounter/MainActivity.kt
+++ app/src/main/java/com/hector/wordcounter/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.hector.wordcounter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val FOLDER_SELECTION_REQUEST_CODE = 100
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +24,16 @@ class MainActivity : AppCompatActivity() {
     private fun initViews() {
 
         selectFolderButton?.setOnClickListener {
-            Toast.makeText(this,"Select Folder",Toast.LENGTH_LONG).show()
+            selectFolder()
         }
+    }
+
+    private fun selectFolder() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE).apply {
+            flags = Intent.FLAG_GRANT_READ_URI_PERMISSION or
+                    Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION
+        }
+        startActivityForResult(intent, FOLDER_SELECTION_REQUEST_CODE)
     }
 
 }
